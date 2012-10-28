89f5aae1d571974d53ed4c9d75e5218d4d9f95da
Todd Freed <todd.freed@gmail.com>
2012-10-28 00:59:41 -0600

    added d operator, ITYPE args, c operator w args

---
+++ liblistwise/op/d/d.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <string.h>
+#include <alloca.h>
+
+#include <listwise/operator.h>
+
+#include "control.h"
+
+/*
+
+d operator - sunDer selected (delete)
+
+NO ARGUMENTS
+
+OPERATION
+
+	1. push an empty list on the stack
+	2. invert the current selection
+	2. foreach item that is selected, or all items on top list
+	   2.1. move that item to the top list
+
+*/
+
+static int op_validate(operation* o);
+static int op_exec(operation*, lstack*, int**, int*);
+
+operator op_desc = {
+	  .type					= OPTYPE_GENERAL
+	, .op_validate	= op_validate
+	, .op_exec			= op_exec
+	, .desc					= "	d - "
+};
+
+int op_validate(operation* o)
+{
+	if(o->argsl)
+		fail("d -- arguments : %d", o->argsl);
+
+	return 1;
+}
+
+int op_exec(operation* o, lstack* ls, int** ovec, int* ovec_len)
+{
+	fatal(lstack_push, ls);
+
+	if(ls->sel.l)
+	{
+		fatal(lstack_ensure, ls, 0, ls->s[1].l - ls->sel.l - 1, -1);
+
+		int i = 0;
+		int x;
+		for(x = ls->s[1].l - 1; x >= 0; x--)
+		{
+			if(ls->sel.sl > (x/8))
+			{
+				if((ls->sel.s[x/8] & (0x01 << (x%8))) == 0)
+				{
+					ls->s[0].s[i++] = ls->s[1].s[x];
+
+					memmove(
+						  &ls->s[1].s[x]
+						, &ls->s[1].s[x+1]
+						, (ls->s[1].l - x - 1) * sizeof(ls->s[0].s[0])
+					);
+				}
+			}
+		}
+
+		ls->s[1].l -= i;
+		ls->s[1].a -= i;
+	}
+	else
+	{
+		typeof(ls->s[0]) T = ls->s[0];
+		ls->s[0] = ls->s[1];
+		ls->s[1] = T;
+	}
+
+	fatal(lstack_sel_clear, ls);
+}
